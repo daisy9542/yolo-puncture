3fab2a05021f8166d37cc7a0e69b66acf9c894fb
daisy9542 <daisy_52036@outlook.com>
2024-07-14 12:37:41 +0800

    Fix the wrong path of weights

--- finetune.py
+++ finetune.py
@@ -1,5 +1,5 @@
 from ultralytics import YOLO
 
-model = YOLO('weights/yolov10x.pt')
+model = YOLO('yolov10x.pt')
 
 model.train(data='datasets/needle/data.yaml', epochs=200, batch=32, imgsz=640, device='0,1', freeze=10) # 大概23个layers, freeze不能超过23
