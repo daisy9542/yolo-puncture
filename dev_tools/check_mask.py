f9de4bcf5464201bf5a24252af23b2bace319245
daisy9542 <daisy_52036@outlook.com>
2024-10-03 14:55:08 +0800

    Add tips for script

--- dev_tools/check_mask.py
+++ dev_tools/check_mask.py
@@ -11,10 +11,12 @@ from yolo_seg.utils.config import get_config
 CONFIG = get_config()
 DATASETS_DIR = CONFIG.PATH.DATASETS_PATH
 
+
 def draw_polygon(image, points):
     height, width = image.shape[:2]
     points = (points * [width, height]).reshape((-1, 1, 2)).astype(np.int32)
     cv2.polylines(image, [points], isClosed=True, color=(0, 255, 0), thickness=2)
+
 
 if __name__ == '__main__':
     images_dir = f"resources/annotations/images"
@@ -25,19 +27,21 @@ if __name__ == '__main__':
     for image_file in image_files:
         image_path = os.path.join(images_dir, image_file)
         label_path = os.path.join(labels_dir, image_file.replace('.jpg', '.txt'))
-    
+        
         image = cv2.imread(image_path)
-    
+        
         with open(label_path, 'r') as f:
             for line in f:
                 values = line.strip().split()
                 class_id = int(values[0])
                 polygon_points = np.array([float(v) for v in values[1:]]).reshape(-1, 2)
                 draw_polygon(image, polygon_points)
-    
+        
+        cv2.putText(image, "Press 'enter' to skip, 'd' to delete image.",
+                    (10, 60), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 0, 0), 2)
         cv2.imshow(f"Image: {image_file}", image)
         key = cv2.waitKey(0)
-    
+        
         if key == ord('d'):
             os.remove(image_path)
             os.remove(label_path)
