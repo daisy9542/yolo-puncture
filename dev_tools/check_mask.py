6d6a8a097478f4f419ce017a7c2429e37d63244a
daisy9542 <daisy_52036@outlook.com>
2024-10-03 15:05:10 +0800

    Add flexible control for script

--- dev_tools/check_mask.py
+++ dev_tools/check_mask.py
@@ -6,11 +6,6 @@ import os
 import cv2
 import numpy as np
 
-from yolo_seg.utils.config import get_config
-
-CONFIG = get_config()
-DATASETS_DIR = CONFIG.PATH.DATASETS_PATH
-
 
 def draw_polygon(image, points):
     height, width = image.shape[:2]
@@ -23,8 +18,12 @@ if __name__ == '__main__':
     labels_dir = f"resources/annotations/labels"
     
     image_files = [f for f in os.listdir(images_dir) if f.endswith('.jpg')]
+    i = 0
+    total = len(image_files)
     
-    for image_file in image_files:
+    while i < total:
+        print(i, image_files[i])
+        image_file = image_files[i]
         image_path = os.path.join(images_dir, image_file)
         label_path = os.path.join(labels_dir, image_file.replace('.jpg', '.txt'))
         
@@ -37,7 +36,7 @@ if __name__ == '__main__':
                 polygon_points = np.array([float(v) for v in values[1:]]).reshape(-1, 2)
                 draw_polygon(image, polygon_points)
         
-        cv2.putText(image, "Press 'enter' to skip, 'd' to delete image.",
+        cv2.putText(image, "Press 'enter': skip, 'd': delete image, 'p': previous image",
                     (10, 60), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 0, 0), 2)
         cv2.imshow(f"Image: {image_file}", image)
         key = cv2.waitKey(0)
@@ -45,5 +44,9 @@ if __name__ == '__main__':
         if key == ord('d'):
             os.remove(image_path)
             os.remove(label_path)
-            print(f"Deleted {image_file} and its label.")
+        elif key == ord('p'):
+            if i > 0:
+                i -= 1
+            continue
+        i+=1
         cv2.destroyAllWindows()
