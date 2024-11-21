b002032b27902fda2d8ca54bfd5e7e91041c9666
daisy9542 <daisy_52036@outlook.com>
2024-11-21 15:22:19 +0800

    Update sub modules

--- deva/inference/custom_eval_args.py
+++
@@ -1,6 +0,0 @@
-from argparse import ArgumentParser
-
-def add_custom_eval_args(parser: ArgumentParser):
-    parser.add_argument("--video_name", type=str, required=True, help="Save folder of the video")
-    parser.add_argument("--enable_reinforce", action='store_true', help="Enable memory reinforce")
-    parser.add_argument("--forward_clip_frames", type=int, default=0, help="Number of frames to forward in-clip Consensus")
